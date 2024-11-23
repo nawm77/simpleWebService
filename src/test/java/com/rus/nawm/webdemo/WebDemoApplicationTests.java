307a19f73609df2555c4e873f288d3219e11b765
i.s.chaynichkov <alpha050603@gmail.com>
2024-11-24 01:24:01 +0300

    init

---
+++ src/test/java/com/rus/nawm/webdemo/WebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rus.nawm.webdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
