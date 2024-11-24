d09e5fb34973d7f5e27696274acc0b40826cffa2
i.s.chaynichkov <alpha050603@gmail.com>
2024-11-24 22:06:53 +0300

    add health controller

---
+++ src/main/java/com/rus/nawm/webdemo/HealthController.java
@@ -0,0 +1,15 @@
+package com.rus.nawm.webdemo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/health")
+@RestController
+public class HealthController {
+  @GetMapping
+  public ResponseEntity<?> getHealth() {
+    return ResponseEntity.ok("I am alive");
+  }
+}
